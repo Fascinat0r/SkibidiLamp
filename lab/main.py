739ee56db384be79ade53cdea20d98ad4a0ad43a
Fascinator <86772638+Fascinat0r@users.noreply.github.com>
2023-12-02 09:10:44 +0300

    Update main.py
    Works with new driver code

--- lab/main.py
+++ lab/main.py
@@ -6,15 +6,14 @@ def main():
    ADDR = 10
    bus = SMBus(0)
    while True:
-      bus.write_i2c_block_data(10, 0, [0, 0, 0xAA])# зааттачить
+      bus.write_i2c_block_data(10, 0xAA, [0, 0])# зааттачить
       time.sleep(1)
       bus.write_i2c_block_data(10, 0, [90, 90])# задать углы
       time.sleep(1)
       bus.write_i2c_block_data(10, 0, [180, 180])
       time.sleep(1)
-      bus.write_i2c_block_data(10, 0, [0, 0, 0xBB])# задетачить
-      #print(angle,' out:' ,bus.read_byte_data(ADDR, 0))
-      time.sleep(0.1)
+      bus.write_i2c_block_data(10, 0xBB, [0, 0,])# задетачить
+      time.sleep(1)
 
 if __name__ == '__main__':
     main()
